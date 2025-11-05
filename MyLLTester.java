06190b1d63d9588fbd48d704496fece9aa644831
LeoWang-LW <yuzhewang2017@gmail.com>
2025-11-05 17:47:01 +0000

    Updates 11/5/25

--- MyLLTester.java
+++ MyLLTester.java
@@ -1,8 +1,12 @@
-public class MyLLTester.java
+public class MyLLTester
 {
   public static void main(String [] args)
   {
-    
+    MyLinkedList Chris = new MyLinkedList(); 
+    System.out.println(Chris.size()); 
+
+    Chris.toString(); 
+
 
   } // end main
   
--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,69 @@
 public class MyLinkedList
 {
+    //field(s): 
+    private ListNode head;
+    private int size; 
 
+    public MyLinkedList(){
+        head = null; 
+        size = 0; 
+    } // end constructor 
 
+    public int size(){
+        return size; 
+    }
+
+    public boolean isEmpty(){
+        if(size == 0){
+            return true; 
+        }
+        else{
+            return false; 
+        }
+    }
+
+    public String toString(){
+        String output = ""; 
+        ListNode current = head; 
+        int position = 0; 
+
+        while(current!= null){
+            output = String.valueOf(position) + ":" + current.getValue();
+            position += 1;
+            current = current.getNext();
+
+        }
+        return output; 
+    }
+    public boolean add(Object newItem){
+        if(head == null){
+            ListNode newNode = new ListNode(null, null);
+            head = newNode;
+        }
+        else{
+            ListNode current = head; 
+            while(current.getNext() != null){
+                current = current.getNext(); 
+                ListNode newNode = new ListNode(null, null);
+                current = newNode;
+            }
+             
+        }
+        size+= 1; 
+        return true; 
+    }
+
+    public boolean addFirst(Object newItem){
+        ListNode newNode = new ListNode(null,null); 
+        newNode = head; 
+        head = newNode; 
+        size += 1; 
+        return true; 
+    }
+
+    public boolean addLast(Object newItem){
+        add(newItem); 
+        return true; 
+    }
 }
+
