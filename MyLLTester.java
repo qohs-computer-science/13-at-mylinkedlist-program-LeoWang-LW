96e8f59cf1511c8cd674d33f4a6b4b42d8d3132c
LeoWang-LW <yuzhewang2017@gmail.com>
2025-11-11 17:32:51 +0000

    Finished 11/11/25

--- MyLLTester.java
+++ MyLLTester.java
@@ -1,21 +1,68 @@
+// leo Wang | 11/11/25 | Pd. 6 | CP3
+
 public class MyLLTester
 {
   public static void main(String [] args)
   {
-    MyLinkedList Chris = new MyLinkedList(); 
-    System.out.println(Chris.size()); 
-    System.out.println(Chris.isEmpty()); 
-    System.out.println(Chris.toString());
-    Chris.add("Meade");
-    Chris.addFirst("Enzo");
-    Chris.addLast("Nick");
-    System.out.println(Chris.size()); 
-    System.out.println(Chris.toString());
-    System.out.println(Chris.get(2)); 
-    System.out.println(Chris.set(2,"Leo")); 
-    System.out.println(Chris.get(2)); 
-    System.out.println(Chris.remove(0)); 
-    System.out.println(Chris.removeLast()); 
+    MyLinkedList chris = new MyLinkedList(); 
+    MyLinkedList kat = new MyLinkedList();
+    MyLinkedList leo = new MyLinkedList();  
+    System.out.println(chris.size()); 
+    System.out.println(chris.isEmpty()); 
+    System.out.println(chris.toString());
+    System.out.println(kat.size()); 
+    System.out.println(kat.isEmpty()); 
+    System.out.println(kat.toString());
+    System.out.println(leo.size()); 
+    System.out.println(leo.isEmpty()); 
+    System.out.println(leo.toString());
+    System.out.println(chris.add("Meade"));
+    System.out.println(chris.addFirst("Enzo"));
+    System.out.println(chris.addLast("Nick"));
+    System.out.println(chris.add("lemons"));
+    System.out.println(chris.add("apples"));
+    System.out.println(chris.add("tissues"));
+    System.out.println(chris.add("programming"));
+    System.out.println(chris.add("Ms. Adams"));
+    System.out.println(chris.add("Seniors"));
+    System.out.println(chris.add("Math"));
+    System.out.println(kat.addFirst("chris"));
+    System.out.println(leo.addLast("kat"));
+    System.out.println(chris.size()); 
+    System.out.println(chris.toString());
+    System.out.println(chris.isEmpty()); 
+    System.out.println(kat.size()); 
+    System.out.println(kat.toString());
+    System.out.println(kat.isEmpty()); 
+    System.out.println(leo.size()); 
+    System.out.println(leo.toString());
+    System.out.println(leo.isEmpty()); 
+
+    System.out.println(chris.get(0)); 
+    System.out.println(chris.get(chris.size() - 1));
+    System.out.println(chris.get(((chris.size() - 1)/2)));
+    
+    System.out.println(chris.set(0, "kat")); 
+    System.out.println(chris.set(chris.size() - 1, "leo"));
+    System.out.println(chris.set(((chris.size() - 1)/2), "Ms. Adams"));
+    System.out.println(chris.toString()); 
+
+    System.out.println(chris.remove(1)); 
+    System.out.println(chris.removeFirst()); 
+    System.out.println(chris.removeLast()); 
+    System.out.println(chris.toString()); 
+    System.out.println(chris.size());
+    System.out.println(chris.isEmpty()); 
+
+    System.out.println(chris.add("CP3")); 
+    System.out.println(chris.get(0)); 
+    System.out.println(chris.set(0, "AP Java")); 
+    System.out.println(chris.toString()); 
+    System.out.println(chris.size());
+    System.out.println(chris.isEmpty()); 
+
+
+
 
   } // end main
   
--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,3 +1,5 @@
+// Leo Wang | 11/11/25 | Pd. 6 | CP3
+
 public class MyLinkedList
 {
     //field(s): 
@@ -11,16 +13,16 @@ public class MyLinkedList
 
     public int size(){
         return size; 
-    }
+    } // end size method 
 
     public boolean isEmpty(){
         if(size == 0){
             return true; 
-        }
+        } // end if 
         else{
             return false; 
-        }
-    }
+        } // end else 
+    } // end isEmpty method
 
     public String toString(){
         String output = ""; 
@@ -28,83 +30,84 @@ public class MyLinkedList
         int position = 0; 
 
         while(current != null){
-            output += position + ":" + current.getValue() + "\n";
+            output += "\n" + position + ":" + current.getValue() + "\n";
             position += 1;
             current = current.getNext();
-        }
+        } // end while loop 
         return output; 
-    }
+    } // end toString method 
+
     public boolean add(Object newItem){
         ListNode newNode = new ListNode(newItem, null); 
         if(head == null){
             head = newNode;
             size++;
             return true; 
-        }
+        } // end if statment 
 
         ListNode temp = head; 
         while(temp.getNext() != null){
             temp = temp.getNext(); 
-        }
+        } // end while loop 
         size++;
         temp.setNext(newNode); 
         return true; 
-    }
+    } // end add method 
 
     public boolean addFirst(Object newItem){
         ListNode newNode = new ListNode(newItem, head); 
         head = newNode; 
         size += 1; 
         return true; 
-    }
+    } // end addFirst method 
 
     public boolean addLast(Object newItem){
         add(newItem); 
         return true; 
-    }
+    } // addLast method 
 
     public Object get(int i){
         int x = 0;
         if(i < 0 || i >= size){
             throw new IndexOutOfBoundsException(); 
-        }
+        } // end if statement 
         ListNode current = head; 
         while( x != i){
             current = current.getNext(); 
             x++;
-        }
+        } // end while loop 
         return current.getValue(); 
-    }
+    } // end get method 
 
     public Object set(int i, Object newValue){
         String oldValue = ""; 
         int x = 0; 
         if(i < 0 || i >= size){
             throw new IndexOutOfBoundsException();
-        }
+        } // end if statement 
         ListNode current = head; 
         while(x != i){
             current = current.getNext(); 
             x++;
-        }
+        } // end while loop 
         oldValue = ((String) current.getValue()); 
         current.setValue(newValue); 
         return oldValue; 
 
-    }
+    } // end set method 
 
     public Object remove(int i){
         if(i < 0 || i >= size){
             throw new IndexOutOfBoundsException();
-        }
+        } // end if statement 
         
         if( i == 0){
             return removeFirst(); 
-        }
+        } // end if statement 
         ListNode current = head; 
         for (int x = 0; x < i - 1; x++){
             current = current.getNext();
-        }
+        } // end for loop 
         ListNode removed = current.getNext(); 
         Object removedValue = removed.getValue(); 
 
@@ -113,31 +116,31 @@ public class MyLinkedList
         size--; 
         return removedValue; 
 
-    }
+    } // end remove method 
 
     public Object removeFirst(){
         if( head == null){
             return null; 
-        }
+        } // end if statement 
         Object removedValue = head.getValue();
         ListNode remove = head; 
         head = head.getNext(); 
         remove.setNext(null); 
         size--; 
         return removedValue; 
-    }
+    } // end removeFirst method 
 
     public Object removeLast(){
         if( head == null){
             return null; 
-        }
+        } // end if statement 
         return remove(size - 1); 
-    }
+    } // end removeLast method
 
     
 
 
 
 
-}
+} // end class 
 
